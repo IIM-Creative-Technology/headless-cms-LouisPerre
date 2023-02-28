75f8b377158466278c1ce9a2d8e977f4f4dbdeee
Louis Perrenot <louis.perrenot@edu.devinci.net>
2023-02-28 21:42:52 +0100

    init files

---
+++ headless-nuxt/pages/index.vue
@@ -0,0 +1,16 @@
+<template >
+    <div>
+        <h1>Hello World</h1>
+        <nuxt-link :to="`/projects/${project.slug}`" v-for="project in projets.data">{{ project.name }}</nuxt-link>
+    </div>
+</template>
+
+<script setup>
+const { find } = useStrapi();
+const { data: projects } = await useAsyncData('projects', () => find('projets', { populate: 'deep' }))
+
+console.log(projects)
+
+</script>
+
+<style scoped></style>
